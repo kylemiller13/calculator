e5ae64c8f8b7ba4f7b85ffb0ecb8d17afff5fe85
Kyle Miller <millerkyle85@gmail.com>
2022-01-19 19:16:04 -0800

    add functions

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,28 @@ const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 const result = add(number1, number2);
 alert(result);
+
+function subtract(number3, number4) {
+  return number3 - number4;
+}
+const number3 = parseInt(prompt("Enter a number:"));
+const number4 = parseInt(prompt("Enter another number:"));
+const final = subtract(number3, number4);
+alert(final);
+
+function multiply(number5, number6) {
+  return number5 * number6;
+}
+const number5 = parseInt(prompt("Enter a number:"));
+const number6 = parseInt(prompt("Enter another number:"));
+const end = multiply(number5, number6);
+alert(end);
+
+function divide(number7, number8) {
+  return number7 / number8;
+}
+const number7 = parseInt(prompt("Enter a number:"));
+const number8 = parseInt(prompt("Enter another number:"));
+const input = divide(number7, number8);
+alert(input);
+
